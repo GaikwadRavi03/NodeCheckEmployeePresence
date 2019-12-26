44bbf18d93a33b405c7561570c5bd236e0639ff7
mvinchankar <mvinchankar18@gmail.com>
2019-12-26 11:16:20 +0530

    Added Code for calculate wages till number of working days or total woring hours per month is reached.

--- employeePresence.js
+++ employeePresence.js
@@ -1,13 +1,16 @@
 let EMP_RATE_PER_HOUR = 20;
 let IS_PART_TIME = 1;
 let IS_FULL_TIME = 2;
-let NUM_OF_WORKING_DAYS = 2;
+let NUM_OF_WORKING_DAYS = 20;
+let MAX_HOURS_IN_MONTH = 10;
 
 let empHrs = 0;
-let empWage = 0;
+let totalEmpHrs = 0;
 let totalEmpWage = 0;
+let totalWorkingDays = 0;
 
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS){
+	totalWorkingDays++; 
 	let empCheck = Math.floor(Math.random() * 10) % 3;
 	switch (empCheck){
 		case IS_FULL_TIME :
@@ -19,7 +22,7 @@ for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
 		default:
 			empHrs = 0;
 	}
-	empWage = empHrs * EMP_RATE_PER_HOUR;
-	totalEmpWage += empWage;
+	totalEmpHrs += empHrs;
 }
-console.log(empWage)
+	totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+console.log(totalEmpWage)
