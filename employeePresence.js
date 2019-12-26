d0ea8f3a054b09ed378960e1c0bbdee6d38ad569
mvinchankar <mvinchankar18@gmail.com>
2019-12-26 10:58:36 +0530

    Added part time Employee wage.

--- employeePresence.js
+++ employeePresence.js
@@ -1,13 +1,16 @@
 let EMP_RATE_PER_HOUR = 20;
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
 
 let empHrs = 0;
 let empWage = 0;
-let isPresent = 1;
 
-let empCheck = Math.floor(Math.random() * 10) % 2;
+let empCheck = Math.floor(Math.random() * 10) % 3;
 
-if (empCheck == 1)
+if (empCheck == IS_FULL_TIME)
 	empHrs = 8;
+else if (empCheck == IS_PART_TIME)
+	empHrs = 4;
 else
 	empHrs = 0;
 
